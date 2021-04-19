5a1fbc4081e0ab4061db66a5c55e9cd177f47516
Harkarman Singh <harkarman.web@gmail.com>
2021-04-20 01:56:47 +0530

    Finish styling

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -6,23 +6,17 @@ module.exports.chatSockets = function (socketServer) {
       credentials: true,
     },
   });
-
   io.sockets.on("connection", function (socket) {
-    console.log("new connection received", socket.id);
-
+    console.log("New connection received", socket.id);
     socket.on("disconnect", function () {
-      console.log("socket disconnected!");
+      console.log("Socket disconnected!");
     });
-
     socket.on("join_room", function (data) {
-      console.log("joining request rec.", data);
-
+      // console.log("Join request received", data);
       socket.join(data.chatroom);
-
       io.in(data.chatroom).emit("user_joined", data);
     });
-
-    // CHANGE :: detect send_message and broadcast to everyone in the room
+    // Detect send_message and broadcast to everyone in the room
     socket.on("send_message", function (data) {
       io.in(data.chatroom).emit("receive_message", data);
     });
